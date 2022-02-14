834da3f8ce774d301b964da1f9eb9c9b8e4aaccf
OfficialMarvin <mkj5388@psu.edu>
2022-02-14 17:21:04 -0500

    fix

--- wordle.py
+++ wordle.py
@@ -1,6 +1,6 @@
 import urllib.request,random
-a,s=list(urllib.request.urlopen('https://www-cs-faculty.stanford.edu/~knuth/sgb-words.txt').read().decode('utf-8').split())[random.randint(0,5756)].upper(),""
-while s!=a:
+a,s=list(urllib.request.urlopen('https://www-cs-faculty.stanford.edu/~knuth/sgb-words.txt').read().decode('utf-8').split())[random.randint(0,5756)],""
+while s!=a.upper():
     g,s=input("guess: "),""
     for i,l in enumerate(a):s+=l.upper() if l==g[i] else "("+g[i].upper()+")" if g[i] in a else "("+g[i].lower()+")"
     print(s)
